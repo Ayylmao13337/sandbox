9f975b826b8c34875dc75dabb8354eaf48e4755e
Trym <hamretrym@gmail.com>
2022-08-23 19:13:07 +0200

    Spillvelger
    Velger spill for meg

---
+++ Spillvelger.py
@@ -0,0 +1,8 @@
+import random, os
+
+spill = random.randint((os.listdir("D:\Steam\steamapps\common")))
+print(spill)
+
+
+spill2 = random.choice(os.listdir("D:\Steam\steamapps\common"))
+print(spill2)
---
+++ idfk.py
@@ -0,0 +1,4 @@
+import random
+
+spill_tilfeldig = random.randint(1 ,  314)
+print(spill_tilfeldig)
